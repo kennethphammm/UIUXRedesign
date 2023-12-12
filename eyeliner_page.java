5597df5e09c9c44ba1403777463f7de829965521
leslydibarra <143983078+leslydibarra@users.noreply.github.com>
2023-12-11 18:50:06 -0800

    Add files via upload

--- eyeliner_page.java
+++ eyeliner_page.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -22,12 +23,15 @@ public class eyeliner_page extends Application {
         homeEyesEyeliner.setStyle("-fx-font-size: 22.4px");
         homeEyesEyeliner.setPadding(new Insets(15, 20, 20, 20));
 
-        Image eyelinerHeaderImage = new Image(getClass().getResource("/Headers/eyelinerheader.jpg").toExternalForm());
+        Image eyelinerHeaderImage = new Image(getClass().getResource("/Headers/eyelinerheader.jpg").toExternalForm(), 1920, 1080, true, true);
         ImageView eyelinerHeader = new ImageView(eyelinerHeaderImage);
 
-        // Set the width of the ImageView to the screen width and customize the height
-        eyelinerHeader.setFitWidth(Screen.getPrimary().getVisualBounds().getWidth());
-        eyelinerHeader.setFitHeight(150);
+        // Directly get the Screen width
+        double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+
+        // Set the width of the ImageView to the screen width and preserve the aspect ratio
+        eyelinerHeader.setFitWidth(screenWidth);
+        eyelinerHeader.setPreserveRatio(true);
 
         Label showFilter = new Label("Show Filter");
         showFilter.setStyle("-fx-font-size: 22.4px;-fx-underline: true;");
@@ -62,88 +66,41 @@ public class eyeliner_page extends Application {
         filterSortBox.setPadding(new Insets(10)); // Set padding
 
         // Insert images with adjusted sizes
-        Image eyeliner1 = new Image(getClass().getResource("/Headers/1.png").toExternalForm(), 300, 300, true, true);
-        Image eyeliner2 = new Image(getClass().getResource("/Headers/2.png").toExternalForm(), 300, 300, true, true);
-        Image eyeliner3 = new Image(getClass().getResource("/Headers/3.png").toExternalForm(), 300, 300, true, true);
-        Image eyeliner4 = new Image(getClass().getResource("/Headers/4.png").toExternalForm(), 300, 300, true, true);
+        double imageWidth = 350;
+        double imageHeight = 350;
+
+        Image eyeliner1 = new Image(getClass().getResource("/Headers/1.png").toExternalForm(), imageWidth, imageHeight, true, true);
+        Image eyeliner2 = new Image(getClass().getResource("/Headers/2.png").toExternalForm(), imageWidth, imageHeight, true, true);
+        Image eyeliner3 = new Image(getClass().getResource("/Headers/3.png").toExternalForm(), imageWidth, imageHeight, true, true);
+        Image eyeliner4 = new Image(getClass().getResource("/Headers/4.png").toExternalForm(), imageWidth, imageHeight, true, true);
 
         ImageView eyeliner1_imageView1 = new ImageView(eyeliner1);
         ImageView eyeliner2_imageView2 = new ImageView(eyeliner2);
         ImageView eyeliner3_imageView3 = new ImageView(eyeliner3);
         ImageView eyeliner4_imageView4 = new ImageView(eyeliner4);
 
-        Image e1_review = new Image(getClass().getResource("/Headers/e1_review.png").toExternalForm(), 200, 200, true, true);
-        Image e2_review = new Image(getClass().getResource("/Headers/e2_review.png").toExternalForm(), 200, 200, true, true);
-        Image e3_review = new Image(getClass().getResource("/Headers/e3_review.png").toExternalForm(), 200, 200, true, true);
-        Image e4_review = new Image(getClass().getResource("/Headers/e4_review.png").toExternalForm(), 200, 200, true, true);
+        Image eyeliner_bottom_page = new Image(getClass().getResource("/category/Bottom_liner_page.png").toExternalForm(), 1920, 1080, true, true);
+        ImageView eyeliner_bottom_page_imageView = new ImageView(eyeliner_bottom_page);
 
-        ImageView e1_review_imageView1 = new ImageView(e1_review);
-        ImageView e2_review_imageView2 = new ImageView(e2_review);
-        ImageView e3_review_imageView3 = new ImageView(e3_review);
-        ImageView e4_review_imageView4 = new ImageView(e4_review);
-
-        // Red heart
-        Image heart = new Image(getClass().getResource("/category/heart.png").toExternalForm(), 20, 20, true, true);
-        Image red_heart = new Image(getClass().getResource("/category/redheart.png").toExternalForm(), 20, 20, true, true);
-
-        ImageView heart_imageView1 = new ImageView(heart);
-        ImageView heart_imageView2 = new ImageView(heart);
-        ImageView heart_imageView3 = new ImageView(heart);
-        ImageView heart_imageView4 = new ImageView(heart);
-
-        // Color options for eyeliner
-        Image e1_color_options = new Image(getClass().getResource("/category/e1_options.png").toExternalForm(), 100, 100, true, true);
-        Image e2_color_options = new Image(getClass().getResource("/category/e2_options.png").toExternalForm(), 100, 100, true, true);
-
-        ImageView e1_color_options_imageView1 = new ImageView(e1_color_options);
-        ImageView e2_color_options_imageView2 = new ImageView(e2_color_options);
-
-        // VBox for hearting the choices
-        VBox eyeliner1_VBox = new VBox();
-        eyeliner1_VBox.getChildren().addAll(heart_imageView1, e1_color_options_imageView1);
-        eyeliner1_VBox.setPadding(new Insets(7));
-
-        HBox eyeliner1_Hbox = new HBox();
-        eyeliner1_Hbox.getChildren().addAll(e1_review_imageView1, eyeliner1_VBox);
-
-        VBox eyeliner2_VBox = new VBox();
-        eyeliner2_VBox.getChildren().addAll(heart_imageView2, e2_color_options_imageView2);
-        eyeliner2_VBox.setPadding(new Insets(7));
-
-        HBox eyeliner2_Hbox = new HBox();
-        eyeliner2_Hbox.getChildren().addAll(e2_review_imageView2, eyeliner2_VBox);
-
-        HBox eyeliner3_Hbox = new HBox();
-        eyeliner3_Hbox.getChildren().addAll(e3_review_imageView3, heart_imageView3);
-        eyeliner3_Hbox.setPadding(new Insets(7));
-
-        HBox eyeliner4_Hbox = new HBox();
-        eyeliner4_Hbox.getChildren().addAll(e4_review_imageView4, heart_imageView4);
-        eyeliner3_Hbox.setPadding(new Insets(7));
-
-        VBox eyeliner1_vBox1 = new VBox();
-        VBox eyeliner2_vBox2 = new VBox();
-        VBox eyeliner3_vBox3 = new VBox();
-        VBox eyeliner4_vBox4 = new VBox();
-        eyeliner1_vBox1.getChildren().addAll(eyeliner1_imageView1, eyeliner1_Hbox);
-        eyeliner2_vBox2.getChildren().addAll(eyeliner2_imageView2, eyeliner2_Hbox);
-        eyeliner3_vBox3.getChildren().addAll(eyeliner3_imageView3, eyeliner3_Hbox);
-        eyeliner4_vBox4.getChildren().addAll(eyeliner4_imageView4, eyeliner4_Hbox);
-
-        HBox hBox2 = new HBox();
-        hBox2.getChildren().addAll(eyeliner1_vBox1, eyeliner2_vBox2, eyeliner3_vBox3, eyeliner4_vBox4);
-        hBox2.setSpacing(40);
-        hBox2.setAlignment(Pos.CENTER);
+        HBox eyeliner_images_hbox = new HBox();
+        eyeliner_images_hbox.getChildren().addAll(eyeliner1_imageView1, eyeliner2_imageView2, eyeliner3_imageView3, eyeliner4_imageView4);
+        eyeliner_images_hbox.setSpacing(20); // Customize the gap between images
+        eyeliner_images_hbox.setPadding(new Insets(10));
 
         // Combine all elements
         VBox eyelinerPageVBox = new VBox();
-        eyelinerPageVBox.getChildren().addAll(homeEyesEyeliner, eyelinerHeader, filterSortBox, hBox2);
+        eyelinerPageVBox.getChildren().addAll(homeEyesEyeliner, eyelinerHeader, filterSortBox, eyeliner_images_hbox, eyeliner_bottom_page_imageView);
         eyelinerPageVBox.setAlignment(Pos.CENTER);
 
         // Set the background color of the root node
         eyelinerPageVBox.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
 
-        Scene scene = new Scene(eyelinerPageVBox, Screen.getPrimary().getVisualBounds().getWidth(), Screen.getPrimary().getVisualBounds().getHeight());
+        // Wrap the VBox in a ScrollPane
+        ScrollPane scrollPane = new ScrollPane(eyelinerPageVBox);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        Scene scene = new Scene(scrollPane, screenWidth, Screen.getPrimary().getVisualBounds().getHeight());
         stage.setScene(scene);
         stage.show();
     }
@@ -152,5 +109,3 @@ public class eyeliner_page extends Application {
         launch(args);
     }
 }
-
-
